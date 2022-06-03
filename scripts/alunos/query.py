9ed1dce1ddddf906ad0310618eeb839cced07584
Francisco José <francisco.jsantana3@gmail.com>
2022-06-03 12:22:00 -0300

    pequenas alteracoes

--- scripts/alunos/query.py
+++ scripts/alunos/query.py
@@ -25,23 +25,55 @@ def query_matrizes():
   matriz = pd.DataFrame(matriz, columns=['SEMESTRE', 'MAT_ALU', 'COD_DISC', 'NOTA', 'FALTAS'])
 
   with open('archives/matriculasDanilo.txt', 'w') as g:
-    g.writelines("INSERT INTO MATRICULAS (SEMESTRE, MAT_ALU, COD_DISC, NOTA, FALTAS, STATUS)\n")
     for _, obj in matriz.iterrows():
+      g.writelines("INSERT INTO MATRICULAS (SEMESTRE, MAT_ALU, COD_DISC, NOTA, FALTAS, STATUS) ")
       if int(obj[3]) >= 7 and int(obj[4]) <= 2:
-          g.writelines(f"VALUES ({obj[0]}, {obj[1]}, {obj[2]}, {obj[3]}, {obj[4]}, 'A'),\n")
+          g.writelines(f" VALUES ({obj[0]}, {obj[1]}, {obj[2]}, {obj[3]}, {obj[4]}, 'A');\n")
       else:
-          g.writelines(f"VALUES ({obj[0]}, {obj[1]}, {obj[2]}, {obj[3]}, {obj[4]}, 'R'),\n")
+          g.writelines(f"VALUES ({obj[0]}, {obj[1]}, {obj[2]}, {obj[3]}, {obj[4]}, 'R');\n")
 
+def matriz_curso():
+  aux = []
+  cod_cursos = [26,35,52,44,131,95,125,103,123,4,13]
+  with open('archives\InsertMatrizesCursos.txt', 'r') as f:
+    for line in f.readlines():
+      new_line = line.replace('Insert into Matrizes_Cursos (COD_DISC,COD_CURSO,PERIODO) values (', '').replace(')', '').replace(';','').replace('\n', '')
+      aux.append(new_line.rsplit(','))
+    
+  with open('archives/InsertMatrizes.txt', 'w') as g:
+    for item in aux:
+      if int(item[1]) not in cod_cursos:
+        print(item[1])
+
+      else:
+        g.writelines(f'Insert into Matrizes_Cursos (COD_DISC,COD_CURSO,PERIODO) values ({item[0]}, {item[1]}, {item[2]});\n')
+  return aux
     
 
-def clean_inserts():
-  with open("archives/alunos.txt", 'r+') as f:
-    f.truncate(-1)
-    
-    
-    
-    
-query_matrizes()
+  
+items = matriz_curso()   
+print(items)
+def disciplinas():
+  aux = []
+  with open('archives\InsertDisciplinas.txt', 'r') as f:
+    for line in f.readlines():
+      new_line = line.replace("Insert into DISCIPLINAS (COD_DISC,NOM_DISC,CARGA_HORARIA) values ('", '').replace(")'", '').replace(';','').replace('\n', '').replace("',", ',')
+      aux.append(new_line.rsplit(','))
+    # print(aux)
+          
+
+  with open('archives/InsertDisicplinasa.txt', 'w') as g:
+    for item in aux:
+      for i in range(len(items)):
+        if str(item[0]) not in items[i][0]:
+          print(item[0])
+
+      else:
+        g.writelines(f'Insert into DISCIPLINAS (COD_DISC,NOM_DISC,CARGA_HORARIA) values ({item[0]}, {item[1]}, {item[2]});\n')
+
+disciplinas()
+
+# matriz_curso()
 # clean_inserts()
 
 # query_alunos()
